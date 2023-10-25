3cf64070c72ebd32c9aba2524f4a41bccce524d8
Amin Jandaghian <jandaghian.amin@gmail.com>
2023-10-25 10:48:59 +0330

    added swagger

--- core/settings.py
+++ core/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY =os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=1))
+DEBUG =bool(os.environ.get("DEBUG", default=1))
 
 ALLOWED_HOSTS =os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "todo.apps.TodoConfig",
     'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
